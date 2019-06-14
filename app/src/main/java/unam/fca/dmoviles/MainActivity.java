c7d951fc6297de000cce458645b012707b776626
pdmcarlos <pdmcarlos159@gmail.com>
2019-06-14 00:42:38 -0500

    se cambio el texto del tv

--- app/src/main/java/unam/fca/dmoviles/MainActivity.java
+++ app/src/main/java/unam/fca/dmoviles/MainActivity.java
@@ -26,7 +26,8 @@ public class MainActivity extends AppCompatActivity {
 
 
         //Para cambiar el texto de tv
-        tv.setText("Hola mundo");
+        tv.setText(R.string.matricula);
+        ;
 
         //Actividades
         //a) Modifica el texto "Hola mundo", pon tu nombre y ejecuta la aplicacion
